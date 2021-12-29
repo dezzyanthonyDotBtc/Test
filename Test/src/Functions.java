4abe024b2f62db81152df11a1413b3a07c8c391e
Dezzy <Dezzy@Dezzy-PC-G>
2021-12-29 13:21:17 +0100

    Some description added

--- Test/src/Functions.java
+++ Test/src/Functions.java
@@ -1,19 +1,32 @@
-
+/**
+ * Diese Klasse erweitert das Programm um ein paar Funktionen
+ * @author Dezzy
+ *
+ */
 public class Functions {
 	
+	/**
+	 * Private boolean um Aktivierungen abzuspeichern
+	 */
 	private boolean activated;
 	
-	
+	/**
+	 * Methode zum setzen der Automatik auf true oder false
+	 * @param active
+	 */
 	public void setAutomatik(boolean active) {
-		
+		//Referenz auf private boolean im Kopf der Klasse
 		this.activated = active;
 	
 	}
 	
+	/**
+	 * Methode zum erhalten/abfragen der Automatik, ob diese true oder false hinterlegt ist
+	 * @param active
+	 */
 	public boolean getAutomatik() {
-		
+		//Referenz auf private boolean im Kopf der Klasse
 		return this.activated;
 	}
-	
 
 }
