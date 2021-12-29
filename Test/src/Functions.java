6d8dff628db5b86d32cd4962152e38d5c602829f
Dezzy <Dezzy@Dezzy-PC-G>
2021-12-29 12:42:21 +0100

    Functions class added - Setter & Getter - AutomationCheck

---
+++ Test/src/Functions.java
@@ -0,0 +1,19 @@
+
+public class Functions {
+	
+	private boolean activated;
+	
+	
+	public void setAutomatik(boolean active) {
+		
+		this.activated = active;
+	
+	}
+	
+	public boolean getAutomatik() {
+		
+		return this.activated;
+	}
+	
+
+}
